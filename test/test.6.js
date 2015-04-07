c1bae689b40c480a4f5ecac58092c1469c300f7a
Davis Kim <davis@blackbird.am>
2015-04-07 13:40:58 -0700

    add tests for handleFields and handleFacetsAndFilters

--- test/test.6.js
+++ test/test.6.js
@@ -10,6 +10,57 @@ describe('Blackbird', () => {
       let instantiateEmptyRequest = () => new Blackbird.Request({});
       instantiateEmptyRequest.should.throw();
     });
+    describe('handleFields', () => {
+      it('should return the input when the input is a string', () => {
+        Blackbird.Request.handleFields('test').should.not.equal('nottest');
+        Blackbird.Request.handleFields('test').should.equal('test');
+      });
+      it('should return the join the input when the input is an array of strings', () => {
+        Blackbird.Request.handleFields(['a']).should.equal('a');
+        Blackbird.Request.handleFields(['a', 'b']).should.equal('a,b');
+      });
+    });
+    describe('handleFacetsAndFilters', () => {
+      let options1 = {
+        field: 'color',
+        operator: '=',
+        value: 'black'
+      };
+      let options2 = {
+        field: 'price',
+        operator: '<',
+        value: '100.00'
+      };
+      let filter1 = new Blackbird.CnfFilter(options1);
+      let filter2 = new Blackbird.CnfFilter(options2);
+      let facet1 = new Blackbird.EnumFacet({
+        field: 'brand',
+        num: 5
+      });
+      let facet2 = new Blackbird.RangeFacet({
+        field: 'price'
+      });
+      it('should return the string form of a Filter when passed a Filter as input', () => {
+        Blackbird.Request.handleFacetsAndFilters(filter1).should.equal('exp=color:black/type=cnf');
+      });
+      it('should return an array of the string forms of Filters when passed an array of Filters', () => {
+        let filters = [filter1, filter2];
+        let handled = Blackbird.Request.handleFacetsAndFilters(filters);
+        handled.should.be.an.Array;
+        handled[0].should.equal('exp=color:black/type=cnf');
+        handled[1].should.equal('exp=price:(:100.00)/type=cnf');
+      });
+      it('should return the string form of a Facet when passed a Facet as input', () => {
+        Blackbird.Request.handleFacetsAndFilters(facet1).should.equal('field=brand/type=enum/num=5');
+      });
+      it('should return an array of the string forms of Facets when passed an array of Facets', () => {
+        let facets = [facet1, facet2];
+        let handled = Blackbird.Request.handleFacetsAndFilters(facets);
+        handled.should.be.an.Array;
+        handled[0].should.equal('field=brand/type=enum/num=5');
+        handled[1].should.equal('field=price/type=range');
+      });
+    });
   });
 
   //Blackbird.Engine
