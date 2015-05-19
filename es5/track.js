77a61746f2cc08e5263039dcf1806622824536ec
Davis Kim <davis@blackbird.am>
2015-05-19 11:28:31 -0700

    add validation to tracking

--- es5/track.js
+++ es5/track.js
@@ -12,12 +12,21 @@ var TrackRequest = function TrackRequest(options) {
   _classCallCheck(this, TrackRequest);
 
   this.type = options.type;
+  if (!this.type) {
+    throw new Error('A tracking request requires a type from the following: click, cart_add, cart_remove, purchase.');
+  }
   this.query = {
     qId: options.qId,
     docIds: options.docIds,
     uId: options.uI, // optional
     sId: options.sId // optional
   };
+  if (!this.query.qId) {
+    throw new Error('A tracking request requires a qId.');
+  }
+  if (!this.query.docIds) {
+    throw new Error('A tracking request requires docIds.');
+  }
 };
 
 function trackRequest(options) {
